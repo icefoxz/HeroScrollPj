d8c9a3c84055cfeda96f28437bc968abbe5ef4fa
MiYan <39160433+MiYanLiang@users.noreply.github.com>
2021-01-23 23:59:08 +0800

    Update loadcfgImpl.bat

--- Conversion/AutoChangeJson/loadcfgImpl.bat
+++ Conversion/AutoChangeJson/loadcfgImpl.bat
@@ -9,7 +9,7 @@ echo off
 ::java -showversion -XX:+PrintCommandLineFlags -XX:+UseG1GC -Xmx6G -classpath lib\kutil.jar;lib\gs.jar;lib\poi-3.9-20121203.jar;lib\poi-ooxml-3.9-20121203.jar;lib\poi-ooxml-schemas-3.9-20121203.jar;lib\log4j-1.2.16.jar;lib\dom4j-1.6.1.jar;lib\xmlbeans-2.3.0.jar i3k.gtool.DataTool --srcdir=datasrc --luadstdir=..\datafiles\lua_scripts\gamedb --dstdir=..\gamedata --server_dstdir=. --ckFile=res.txt --nthread=4
 if not errorlevel 0 goto p
 pylib\python\python.exe pylib\export.py datasrc ..\..\SanGuoHuiJuanPj\Assets\Resources\Jsons\
-pylib\python\python.exe pylib\export.py datasrc ..\AutoChangeJson\gamedb\Jsons\
+::pylib\python\python.exe pylib\export.py datasrc ..\AutoChangeJson\gamedb\Jsons\
 ::..\script\gamedb\
 ::call setTimeZone.bat %DEFAULT_TIME_ZONE%
 
