711230511909a800b20c653ae44eadee92fe5a60
Liu <liuyongbin09250016@outlook.com>
2021-03-22 13:18:41 +0800

    合并

--- Server/XlsxToJsonConvert.bat
+++ Server/XlsxToJsonConvert.bat
@@ -1,4 +1,4 @@
 echo off
-.\XlsxToJson1.01\xlsxtojson.exe HeroDataTable.xlsx ..\Uploader\jsons
+.\xlsxtojson\xlsxtojson.exe HeroDataTable.xlsx ..\Uploader\jsons
 xcopy Uploader\jsons\*.* ..\SanGuoHuiJuanPj\Assets\Resources\Jsons\
 pause
