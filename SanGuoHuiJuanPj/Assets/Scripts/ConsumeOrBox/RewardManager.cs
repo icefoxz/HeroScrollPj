62730c79864deb9442c27e7ee56c7725851644cd
Leo Vun <icefoxz@msn.com>
2021-03-26 13:03:07 +0800

    修复卡牌存档用错类型的bug

--- SanGuoHuiJuanPj/Assets/Scripts/ConsumeOrBox/RewardManager.cs
+++ SanGuoHuiJuanPj/Assets/Scripts/ConsumeOrBox/RewardManager.cs
@@ -114,8 +114,27 @@ public class RewardManager : MonoBehaviour
     /// <param name="cardId">具体id</param>
     /// <param name="chips">碎片数量</param>
     /// <returns></returns>
-    public void RewardCard(GameCardType cardType, int cardId, int chips) =>
-        PlayerDataForGame.instance.hstData.heroSaveData.GetOrInstance(cardId, cardType, 0).chips += chips;
+    public void RewardCard(GameCardType cardType, int cardId, int chips)
+    {
+        switch (cardType)
+        {
+            case GameCardType.Hero:
+                PlayerDataForGame.instance.hstData.heroSaveData.GetOrInstance(cardId, cardType, 0).chips += chips;
+                break;
+            case GameCardType.Tower:
+                PlayerDataForGame.instance.hstData.towerSaveData.GetOrInstance(cardId, cardType, 0).chips += chips;
+                break;
+            case GameCardType.Trap:
+                PlayerDataForGame.instance.hstData.trapSaveData.GetOrInstance(cardId, cardType, 0).chips += chips;
+                break;
+            case GameCardType.Spell:
+            case GameCardType.Soldier:
+            case GameCardType.Base:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(cardType), cardType, null);
+        }
+    }
 
 
     //卡牌和权重类
